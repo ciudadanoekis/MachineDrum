7e6305b53ed777ac832cbcdbd4ee8b745b4ae576
ciudadano ekis <ciudadanoekis@hotmail.com>
2023-07-09 17:16:38 +0200

    Gain and glide control

--- src/main/java/machine_drum/Hello_Beads.java
+++ src/main/java/machine_drum/Hello_Beads.java
@@ -3,6 +3,8 @@ package machine_drum;
 import net.beadsproject.beads.core.AudioContext;
 import net.beadsproject.beads.core.io.JavaSoundAudioIO;
 import net.beadsproject.beads.data.Buffer;
+import net.beadsproject.beads.ugens.Gain;
+import net.beadsproject.beads.ugens.Glide;
 import net.beadsproject.beads.ugens.WavePlayer;
 
 public class Hello_Beads {
@@ -12,10 +14,30 @@ public class Hello_Beads {
         jsaio.selectMixer(3);
         AudioContext ac = new AudioContext(jsaio);
 
+        //create a sine generator
         WavePlayer wp = new WavePlayer(ac,440.0f, Buffer.SINE);
-        ac.out.addInput(wp);
+
+        //create a Glide to control the gain 5000ms ramp time
+        Glide gainGlide = new Glide(ac, 0.0f, 5000.0f);
+
+        //create a Gain to control the sine wave volume
+
+        Gain sineGain = new Gain(ac, 1, gainGlide);
+
+        //add the wave generator to the gain
+        sineGain.addInput(wp);
+
+        //add the Gain as an input to the master output, ac.out
+        ac.out.addInput(sineGain);
+
+
         ac.start();
+
+        //ramp the gain to 0.9f
+        gainGlide.setValue(0.9f);
     }
+
+
     public static void main(String[] args) {
        macos();
     }
